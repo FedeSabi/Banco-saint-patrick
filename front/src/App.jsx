4c41d4627179f1bac4ca6856de015e4a56e3d222
Federico Sabiche <fsabiche>
2024-02-13 20:19:47 -0300

    se creo el browserRouter

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
 
   return (
-    <div>
-      <h1>Hello world</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element=''/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 

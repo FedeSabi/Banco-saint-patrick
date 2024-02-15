07cf9aa34ef3b9eede55bc57e2181c4f248d3b42
Federico Sabiche <fsabiche>
2024-02-15 17:32:24 -0300

    imagenes seccion beneficios

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,4 +1,3 @@
-import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -6,10 +5,12 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element=''/>
+        <Route path='/' element={<div className="text-center font-bold text-5xl mt-10 text-red-600">Home</div>}/>
       </Routes>
     </BrowserRouter>
   )
 }
 
-export default App
+export default App;
+
+

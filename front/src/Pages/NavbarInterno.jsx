1fa0cc3ba88be03b605ae6e010d9b2b1e19f183d
Federico Sabiche <fsabiche>
2024-04-13 09:54:55 -0300

    se agregaron acentos faltantes

--- front/src/Pages/NavbarInterno.jsx
+++ front/src/Pages/NavbarInterno.jsx
@@ -1,11 +1,12 @@
 function NavbarInterno() {
   return (
-    <div className=" flex max-w-full h-12 bg-customGreen border-customYellow border-2 justify-around items-center font-semibold">
-    <a href="#" className="text-2xl text-customYellow hover:text-white">Institucional</a>
-    <a href="#" className="text-2xl text-customYellow hover:text-white">Productos y servicios</a>
-    <a href="#" className="text-2xl text-customYellow hover:text-white">Canales de atencion</a>
-    <a href="#" className="text-2xl text-customYellow hover:text-white">Unite a Saint Patrick</a>
-    </div>
+    <div className="flex max-w-full h-12 bg-customGreen border-customYellow border-2 justify-around items-center font-semibold">
+  <a href="#" className="text-2xl text-customYellow hover:text-white">Institucional</a>
+  <a href="#" className="text-2xl text-customYellow hover:text-white">Productos y servicios</a>
+  <a href="#" className="text-2xl text-customYellow hover:text-white">Canales de atención</a>
+  <a href="#" className="text-2xl text-customYellow hover:text-white">Únete a Saint Patrick</a>
+</div>
+
   )
 }
 export default NavbarInterno

627c1176eb1c2d40185e7bcdb2aaae19925d65e7
Federico Sabiche <fsabiche>
2024-05-16 16:24:07 -0300

    se hizo responsive seccion institucional

--- front/src/Pages/NavbarInterno.jsx
+++ front/src/Pages/NavbarInterno.jsx
@@ -1,10 +1,10 @@
 function NavbarInterno() {
   return (
     <div className="flex max-w-full h-12 bg-customGreen border-customYellow border-2 justify-around items-center font-semibold">
-  <a href="/institucional" className="text-2xl text-customYellow hover:text-white">Institucional</a>
-  <a href="/ProductosYservicios" className="text-2xl text-customYellow hover:text-white">Productos y servicios</a>
-  <a href="/CanalesDeAtencion" className="text-2xl text-customYellow hover:text-white">Canales de atención</a>
-  <a href="/UniteASaintPatrick" className="text-2xl text-customYellow hover:text-white">Unete a Saint Patrick</a>
+  <a href="/institucional" className="xl:text-2xl md:text-lg  text-customYellow hover:text-white">Institucional</a>
+  <a href="/ProductosYservicios" className="xl:text-2xl md:text-lg text-customYellow hover:text-white">Productos y servicios</a>
+  <a href="/CanalesDeAtencion" className="xl:text-2xl md:text-lg text-customYellow hover:text-white">Canales de atención</a>
+  <a href="/UniteASaintPatrick" className="xl:text-2xl md:text-lg text-customYellow hover:text-white">Unete a Saint Patrick</a>
 </div>
 
   )

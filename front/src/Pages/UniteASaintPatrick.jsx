0d84d94e8d65b0c670d5668f106e69400ec2316b
Federico Sabiche <fsabiche>
2024-05-17 20:56:56 -0300

    se corrigio errores xl de unete a saint patrick

--- front/src/Pages/UniteASaintPatrick.jsx
+++ front/src/Pages/UniteASaintPatrick.jsx
@@ -11,13 +11,13 @@ const UniteASaintPatrick = () => {
           <img src={Persona} className='xs: ml-28'/>
         </div>
         {/*divisor texto */}
-        <div className=' text-customGreen xs:w-[380px] xl:w-[520px] flex flex-col xl:justify-around xs:ml-2 mt-20 gap-2'>
+        <div className=' text-customGreen xs:w-[380px] xl:w-[520px] flex flex-col xl:justify-around xs:ml-2 xl:ml-6 mt-20 gap-2'>
           <div className='xs:text-2xl xs:mb-10 xl:text-5xl font-bold  xs:text-center xl:text-left'>
             <h1 className='h-10'>Sumate a nuestro equipo Saint Patrick</h1>
           </div>
           <div className='font-bold mb-20' >
-            <p className='xs:text-lg xs:text-center xl:text-3xl mb-10'>Postulate a nuestras busquedas y potencia lo mejor de vos</p>
-            <p className='xs:text-lg  xs:text-center xl:text-3xl'>Banco Saint Patrick te alienta a pensar en grande.</p>
+            <p className='xs:text-lg xs:text-center xl:text-left xl:text-3xl mb-10'>Postulate a nuestras busquedas y potencia lo mejor de vos</p>
+            <p className='xs:text-lg  xs:text-center xl:text-left xl:text-3xl'>Banco Saint Patrick te alienta a pensar en grande.</p>
           </div>
           <div className='xs:w-[auto] xs:text-center xl:w-[520px]'>
             <button className='bg-amber-500 xs:mb-10 xs:w-[200px] xl:w-[500px] xs:h-12 xl:h-16 xs:text-lg xl:text-3xl font-semibold rounded-xl  text-black '>Conocer propuestas</button>
